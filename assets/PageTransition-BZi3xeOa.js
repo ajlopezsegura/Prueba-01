40f5ee9ace414a3ad425d6f15931d9b5958777dd
ajlopezsegura <ajlopezsegura@users.noreply.github.com>
2026-08-01 07:54:35 +0000

    deploy: 23e8b64bab9059de4ea266753caa50b3a32c9c9f

---
+++ assets/PageTransition-BZi3xeOa.js
@@ -0,0 +1,1 @@
+import{j as a,m as n}from"./index-CBjg8Vhv.js";function e({children:i,className:t=""}){return a.jsx(n.div,{initial:{opacity:0},animate:{opacity:1},exit:{opacity:0},transition:{duration:.2,ease:"easeInOut"},className:t,style:{position:"fixed",inset:0},children:i})}export{e as P};
